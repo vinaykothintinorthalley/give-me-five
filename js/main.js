14fd7f4efb8e9f9df45db1f07035c43b7b86232f
Vinay Kumar <vinaykumar@Vinays-MacBook-Pro.local>
2024-06-17 15:32:35 +0530

    linkedin share functionality updated in blog details pages

--- js/main.js
+++ js/main.js
@@ -110,3 +110,14 @@ function submitForm(){
   // Optional: Alert the copied text
   //alert('Copied the text: ' + tempInput.value);
 });
+
+// JavaScript to handle LinkedIn share functionality
+document.getElementById('linkedinShareButton').addEventListener('click', function(event) {
+  event.preventDefault();
+  
+  const urlToShare = encodeURIComponent(window.location.href);
+  
+  const shareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${urlToShare}`;
+  
+  window.open(shareUrl, 'linkedinShareWindow', 'height=450,width=550,top=' + (screen.height / 2 - 225) + ',left=' + (screen.width / 2 - 275) + ',toolbar=0,location=0,menubar=0,directories=0,scrollbars=0');
+});
